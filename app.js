01d22d5d2c338f89a2ea7547754e2224e096197d
trizzi <patrickakhamiogu@gmail.com>
2021-08-28 12:26:04 +0100

    Included the random user API

--- app.js
+++ app.js
@@ -10,8 +10,16 @@ const app = Vue.createApp({
     };
   },
   methods: {
-    getUser() {
-      console.log(this.firstName);
+    async getUser() {
+      const res = await fetch('https://randomuser.me/api/');
+      const { results } = await res.json;
+      console.log(results);
+
+      (this.firstName = results[0].name.first),
+        (this.lastName = results[0].name.last),
+        (this.email = results[0].email),
+        (this.gender = results[0].gender),
+        (this.picture = results[0].picture.large);
     },
   },
 });
