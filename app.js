a34d46f5127c95965b6908a6b1e01bf13a560c22
trizzi <patrickakhamiogu@gmail.com>
2021-08-30 10:54:13 +0100

    worked on css issues

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const app = Vue.createApp({
     async getUser() {
       const res = await fetch('https://randomuser.me/api');
       const { results } = await res.json();
-      console.log('adnfjdfkjfkjab', results);
 
       (this.firstName = results[0].name.first),
         (this.lastName = results[0].name.last),
